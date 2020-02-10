bc72014f6bbe7cb6e935d28793cba8c2ba4301d4
47115025 <aneta.popelova@gmail.com>
2020-02-10 13:14:36 +0100

    jupytext

---
+++ lekce1/prikazovaRadka.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3.7.1 64-bit
+#     name: python37164bit7f0e7b598a744d1097a1b195bb1e9d19
+# ---
+
+# *Zakladni prikazy v prikazove radce (cmd)*
+# pro Windows
+#
+# whoami # prihlasovaci jmeno  
+# # cd # current directory / change directory    
+# dir # vypíše, co aktuální adresář obsahuje  
+# # mkdir # vytvoreni adresare  
+# # cd .. # nadrazeny adresare  
+# # rmdir # remove directory  
+# # copy # zkopírování souboru (copy puvodni.txt kopie.txt)  
+# del # mazani souboru  
+# exit # ukonceni	
+#   
+#     
+#       
+# *Virtualni prostredi*  
+# py -3 -m venv venv # vytvoren  
+# venv\Scripts\activate # aktivuj  
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+#
+#
+#
+#
+
+
