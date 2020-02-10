280d828f25eba4153152c62ea93c45a5a7604f61
47115025 <aneta.popelova@gmail.com>
2020-02-10 13:49:19 +0100

    lekce 2

---
+++ lekce2/promenne.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+# ---
+
+# +
+#  obvod a obsah čtverce se stranou 356 cm
+print('Obvod čtverce se stranou 356 cm je', 4 * 356, 'cm')
+print('Obsah čtverce se stranou 356 cm je', 356 * 356, 'cm2')
+
+# obsah a obvod čtverce o straně 123 cm
+print('Obvod čtverce se stranou 123 cm je', 4 * 123, 'cm')
+print('Obsah čtverce se stranou 123 cm je', 123 * 123, 'cm2')
+# -
+
+strana = 123
+print('Obvod čtverce se stranou', strana, 'je', 4 * strana, 'cm')
+print('Obsah čtverce se stranou', strana, 'je', strana * strana, 'cm2')
+
+# +
+## nacitani vstupu
+
+# Chceš-li načíst text, použij:
+promenna = input('Zadej text: ')
+
+# Chceš-li načíst celé číslo, použij:
+promenna = int(input('Zadej číslo: '))
+
+# Chceš-li načíst desetinné číslo, použij:
+promenna = float(input('Zadej číslo: '))
+
+
+# -
+
+
---
+++ lekce2/prvniProgram.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3.7.1 64-bit
+#     name: python37164bit7f0e7b598a744d1097a1b195bb1e9d19
+# ---
+
+3 + 4
+
+print("Ahoj svete!")
+
+print(1)
+print(1, 2, 3)
+print(1 + 1)
+print(3 * 8)
+print(10 - 2.2)
+print(3 + (4 + 6) * 8 / 2 - 1)
+print('*' * 80)
+print("Ahoj" + " " + "PyLadies!")
+print("Součet čísel 3 a 8 je", 3 + 8)
+print('Máma má mísu')
+
+
